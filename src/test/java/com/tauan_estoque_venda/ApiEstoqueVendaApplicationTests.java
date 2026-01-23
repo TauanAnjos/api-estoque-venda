278d96a04224422c5a59c37c3d66e9c010cb0b89
tauandosanjosnascimento <tauandosanjosnascimento@gmail.com>
2026-01-23 14:34:05 -0300

    chore: initial project setup

---
+++ src/test/java/com/tauan_estoque_venda/ApiEstoqueVendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tauan_estoque_venda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEstoqueVendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
